35730ff3b26079715a78dd30f1df5356f87a8ebb
tsah01 <tus6@pitt.edu>
2021-06-23 12:06:36 -0400

    initial commit

--- JavaAssignments/TASK 7: PROGRAMMING QUESTIONS/CountOccurrencesOfEachCharacter.java
+++ JavaAssignments/TASK 7: PROGRAMMING QUESTIONS/CountOccurrencesOfEachCharacter.java
@@ -1,44 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Task7;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * Task:Program to Count occurrences of each character in a string in java? 
+ *
+ * @author Tula Sah
+ * Date: 9th June, 2021 
+ * File name: CountOccurrencesOfEachCharacter.java 
+ * Compile: javac CountOccurrencesOfEachCharacter.java
+ * Run: java CountOccurrencesOfEachCharacter
+ */
 
 public class CountOccurrencesOfEachCharacter {
 
-    
     public static void main(String[] args) {
-           Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String input = scan.nextLine();
         System.out.println("The given string is: " + input);
-        //first filterout how many unique letter is present the user input
-        //char[]uniqueCharacter = new char[input.length()];
-        Set<Character> uniqueCharacter = new HashSet<>();
-        for(int i =0; i<input.length(); i++){
-            uniqueCharacter.add(input.charAt(i));
-        }
-        System.out.println("the unique letters are: " + uniqueCharacter);
-        //uniqueCharacter.toArray();
-        //now check each character appearance in original input
-        int count=0;
-        for(int j = 0; j< uniqueCharacter.size(); j++){
-            //(int j = 0; j< uniqueCharacter.size(); j++)
-            for(int i =0; i< input.length(); i++){
-                if(input.charAt(i) == uniqueCharacter.toString().charAt(j)){
-                   count++; 
-                }
-                System.out.println("character " + uniqueCharacter.toString().charAt(j) + " repeated " + count);
+        //create a character array of input string
+        char[] inputcharacter = input.toCharArray();
+
+        HashMap<Character, Integer> hashmap = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < inputcharacter.length; i++) {
+            //if the character present in hashmap then put key and value increased by 1 into the hashmap
+            if (hashmap.containsKey(inputcharacter[i])) {
+                hashmap.put(inputcharacter[i], hashmap.get(inputcharacter[i]) + 1);
+            } else {
+                //else  put key and value as 1 into the hashmap
+                hashmap.put(inputcharacter[i], 1);
             }
-            
         }
-        
+        //printing hashmap
+        for (Map.Entry entry : hashmap.entrySet()) {
+            System.out.println(entry.getKey() + " is repeated " + entry.getValue() + " times ");
+        }
+
     }
-    
+
 }
